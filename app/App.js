13c4ebcabacba6c2f8573e57c8563580c35f75d2
SergeiKiss <kiselev_s.d.98@mail.ru>
2024-10-25 07:53:35 +0300

    feat: make Pull To Refresh mechanism in the list

--- app/App.js
+++ app/App.js
@@ -1,21 +1,38 @@
-import React from "react";
-import { SafeAreaView, FlatList } from "react-native";
-import ProgrammingLanguageItem from "./components/ProgrammingLanguageItem.js";
-import langs from "./LanguageList.js";
+import React, { useState } from "react";
+import { SafeAreaView, FlatList, RefreshControl } from "react-native";
+import ProgrammingLanguageItem from "./components/ProgrammingLanguageItem";
+import initialLangs from "./LanguageList.js";
 
 const App = () => {
+  const [langs, setLangs] = useState(initialLangs);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+
+    setTimeout(() => {
+      setLangs(initialLangs);
+      setRefreshing(false);
+    }, 2000);
+  };
+
+  const renderItem = ({ item }) => (
+    <ProgrammingLanguageItem
+      logo={item.logo}
+      name={item.lang}
+      experience={item.experience}
+    />
+  );
+
   return (
     <SafeAreaView>
       <FlatList
         data={langs}
-        renderItem={({ item }) => (
-          <ProgrammingLanguageItem
-            logo={item.logo}
-            name={item.lang}
-            experience={item.experience}
-          />
-        )}
+        renderItem={renderItem}
         keyExtractor={(item) => item.id}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
     </SafeAreaView>
   );
